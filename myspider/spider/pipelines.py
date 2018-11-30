b985f1ce297a1bc034f879645f77cb2ea2f4abec
limit <limit@qq.com>
2018-11-30 08:23:41 +0800

    fix problem

---
+++ myspider/spider/pipelines.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import re
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+from myspider.db import JobModel
+from myspider.db import Session
+
+class MyspiderPipeline(object):
+    def process_item(self, item, spider):
+        item['city'] = item['city'].split('.')[0]
+        item['salary_lower'] = int(item['salary_lower'].strip('k'))
+        item['salary_upper'] = int(item['salary_upper'].strip('k'))
+        exp_lower,exp_upper = 0,0
+        m = re.match(r'[^/d]*(/d+)-(/d+)',item['experience'])
+        if m is not None :
+            try:
+                exp_lower = int(m.group(1))
+                exp_upper = int(m.group(2))
+            except :
+                exp_lower = exp_upper = int.(m.group(1))
+
+        tags = ''.join(item['tags'])
+        jobs = JobModel(
+            title = item['title'],
+            cite = item['city'],
+            salary_lower = item['salary_lower'],
+            salary_upper = item['salary_upper'],
+            experience_lower = exp_lower,
+            experience_upper = exp_upper,
+            education = item['education'],
+            tags = tags,
+            company = item['company']
+                )
+        self.session.add(jobs)
+        return item
+
+    def open_spider(self,spider):
+        self.session = Session()
+
+    def close_spider(self,spider):
+        self.session.commit()
+        self.session.close()
